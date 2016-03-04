4a466c3d97a81577ae7d1485f41c8defcc560c67
Bogdan Didenko <bogdan@webspellchecker.net>
2016-03-04 09:19:14 +0000

    #78224 CKEditor improvements and refactoring of our plugins / (ckEditor4,  ckEditor3, TinyMCE3, TinyMCE4) plugins added check on define scayt Instance in reloadMarkup method
    git-svn-id: https://dev01.webspellchecker.net:8443/SVN/Editors/TinyMCE4/trunk/js/tinymce/plugins/scayt@8230 462193d8-cd1d-f144-b59b-5446b0a36879

--- plugin.js
+++ plugin.js
@@ -171,12 +171,14 @@
 		};
 		// backward compatibility if version of scayt app < 4.8.3
 		var reloadMarkup = function(scaytInstance) {
-			if (scaytInstance.reloadMarkup) {
-				scaytInstance.reloadMarkup();
-			} else {
-				console.warn('Note: you are using new version of SCAYT plug-in. It is recommended to upgrade WebSpellChecker.net to version 4.8.3 Contact us: '+
-					'https://www.webspellchecker.net/contact-us.html');
-				scaytInstance.fire('startSpellCheck');
+			if(scaytInstance){
+				if (scaytInstance.reloadMarkup) {
+					scaytInstance.reloadMarkup();
+				} else {
+					console.warn('Note: you are using new version of SCAYT plug-in. It is recommended to upgrade WebSpellChecker.net to version 4.8.3 Contact us: '+
+						'https://www.webspellchecker.net/contact-us.html');
+					scaytInstance.fire('startSpellCheck');
+				}
 			}
 		};
 		return {
